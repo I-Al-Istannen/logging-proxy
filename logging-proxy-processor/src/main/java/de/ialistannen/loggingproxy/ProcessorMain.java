f3cc3ff39bbf43450e1de3f423afc9ea8db37462
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2023-10-08 19:02:09 +0200

    Use string templating instead of spoon

--- logging-proxy-processor/src/main/java/de/ialistannen/loggingproxy/ProcessorMain.java
+++ logging-proxy-processor/src/main/java/de/ialistannen/loggingproxy/ProcessorMain.java
@@ -1,15 +1,11 @@
 package de.ialistannen.loggingproxy;
 
 import com.google.auto.service.AutoService;
-import com.sun.source.util.TreePath;
-import com.sun.source.util.Trees;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.io.Writer;
-import java.lang.reflect.Method;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
@@ -19,12 +15,6 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 import javax.tools.JavaFileObject;
-import spoon.Launcher;
-import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtType;
-import spoon.reflect.declaration.CtTypeParameter;
-import spoon.reflect.visitor.filter.TypeFilter;
-import spoon.support.compiler.VirtualFile;
 
 @SupportedAnnotationTypes("de.ialistannen.loggingproxy.GenerateTestProxy")
 @SupportedSourceVersion(SourceVersion.RELEASE_21)
@@ -35,30 +25,20 @@ public class ProcessorMain extends AbstractProcessor {
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
     try {
       for (Element element : roundEnv.getElementsAnnotatedWith(GenerateTestProxy.class)) {
-        Trees trees = Trees.instance(jbUnwrap(ProcessingEnvironment.class, processingEnv));
-        TreePath path = trees.getPath(element);
+        if (!(element instanceof TypeElement type)) {
+          processingEnv.getMessager().printError("Found annotated non-type", element);
+          continue;
+        }
 
-        Launcher launcher = new Launcher();
-        launcher.getEnvironment().setAutoImports(true);
-        launcher.getEnvironment().setNoClasspath(true);
-        launcher.getEnvironment().setComplianceLevel(17);
-        launcher.addInputResource(new VirtualFile(
-          path.getCompilationUnit().getSourceFile().getCharContent(false).toString(),
-          path.getCompilationUnit().getSourceFile().toUri().getPath()
-        ));
-        launcher.getEnvironment().setInputClassLoader(getClass().getClassLoader());
-        CtModel model = launcher.buildModel();
+        String generated = new TextblockProcessor().forType(
+          (TypeElement) element,
+          processingEnv.getElementUtils().getPackageOf(element)
+        );
 
-        for (CtType<?> type : model.getElements(new TypeFilter<>(CtType.class))) {
-          if (type instanceof CtTypeParameter) {
-            continue;
-          }
-          CtType<?> generatedProxy = new ProxyGenerator(type).generateProxy();
-          JavaFileObject sourceFile = processingEnv.getFiler()
-            .createSourceFile(generatedProxy.getQualifiedName(), element);
-          try (Writer writer = sourceFile.openWriter()) {
-            writer.write(generatedProxy.toStringWithImports());
-          }
+        JavaFileObject sourceFile = processingEnv.getFiler()
+          .createSourceFile(type.getQualifiedName() + "Proxy", element);
+        try (Writer writer = sourceFile.openWriter()) {
+          writer.write(generated);
         }
 
         processingEnv.getMessager().printMessage(Kind.OTHER, element.toString());
@@ -70,15 +50,4 @@ public class ProcessorMain extends AbstractProcessor {
     return true;
   }
 
-  // So running from within Jetbrains IDEs works.
-  private static <T> T jbUnwrap(Class<? extends T> iface, T wrapper) {
-    T unwrapped = null;
-    try {
-      final Class<?> apiWrappers = wrapper.getClass().getClassLoader().loadClass("org.jetbrains.jps.javac.APIWrappers");
-      final Method unwrapMethod = apiWrappers.getDeclaredMethod("unwrap", Class.class, Object.class);
-      unwrapped = iface.cast(unwrapMethod.invoke(null, iface, wrapper));
-    } catch (Throwable ignored) {
-    }
-    return unwrapped != null ? unwrapped : wrapper;
-  }
 }
---
+++ logging-proxy-processor/src/main/java/de/ialistannen/loggingproxy/TextblockProcessor.java
@@ -0,0 +1,137 @@
+package de.ialistannen.loggingproxy;
+
+import static java.util.stream.Collectors.joining;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+
+public class TextblockProcessor {
+
+  private static final String METHOD_TEMPLATE_NON_VOID = """
+    public {returnType} {name}({paramDecl}) {
+      $testLogger$.onMethodCallStart("{name}", {paramNames});
+      {returnType} result = {receiver}.{name}({paramNames});
+      $testLogger$.onMethodReturn("{name}", result);
+      return result;
+    }
+    """;
+  private static final String METHOD_TEMPLATE_VOID = """
+    public void {name}({paramDecl}) {
+      $testLogger$.onMethodCallStart("{name}", {paramNames});
+      {receiver}.{name}({paramNames});
+      $testLogger$.onMethodReturn("{name}", null);
+    }
+    """;
+  private static final String CONSTRUCTOR_TEMPLATE_WITH_INNER = """
+    public {innerName}Proxy({loggerType} logger, {innerName} inner) {
+      this.$testLogger$ = logger;
+      this.$inner$ = inner;
+    }
+    """;
+  private static final String CONSTRUCTOR_TEMPLATE_WITHOUT_INNER = """
+    public {innerName}Proxy({loggerType} logger) {
+      this.$testLogger$ = logger;
+    }
+    """;
+
+  public String forType(TypeElement element, PackageElement packageOf) {
+    String name = element.getSimpleName().toString() + "Proxy";
+    String methods = getProxiedMethods(element).stream()
+      .sorted(Comparator.comparing(ExecutableElement::getSimpleName, Comparator.comparing(Objects::toString)))
+      .map(this::templateMethod)
+      .collect(joining("\n"));
+    String fields = getFields(element);
+    String constructor = getConstructor(element);
+    String imports = getImports();
+
+    return """
+      package {package};
+
+      {imports}
+
+      public class {name} {
+      {fields}
+            
+      {constructor}
+            
+      {methods}
+      }
+      """
+      .replace("{package}", packageOf.getQualifiedName())
+      .replace("{imports}", imports)
+      .replace("{name}", name)
+      .replace("{methods}", methods.indent(2).stripTrailing())
+      .replace("{fields}", fields.indent(2).stripTrailing())
+      .replace("{constructor}", constructor.indent(2).stripTrailing());
+  }
+
+  private String getImports() {
+    return "import {testLogger};".replace("{testLogger}", TestLogger.class.getName());
+  }
+
+  private List<ExecutableElement> getProxiedMethods(TypeElement element) {
+    return element.getEnclosedElements().stream().filter(it -> it.getKind() == ElementKind.METHOD)
+      .filter(it -> it.getAnnotation(GenerateTestProxy.ProxyMethod.class) != null)
+      .map(it -> (ExecutableElement) it)
+      .toList();
+  }
+
+  private String templateMethod(ExecutableElement element) {
+    String methodTemplate = element.getReturnType().getKind() == TypeKind.VOID
+      ? METHOD_TEMPLATE_VOID
+      : METHOD_TEMPLATE_NON_VOID;
+
+    String paramNames = element.getParameters()
+      .stream()
+      .map(it -> it.getSimpleName().toString())
+      .collect(joining(", "));
+    String paramDecl = element.getParameters()
+      .stream()
+      .map(it -> it.asType() + " " + it.getSimpleName())
+      .collect(joining(", "));
+
+    String receiver = element.getModifiers().contains(Modifier.STATIC)
+      ? element.getEnclosingElement().getSimpleName().toString()
+      : "$inner$";
+
+    methodTemplate = methodTemplate.replace("{name}", element.getSimpleName())
+      .replace("{returnType}", element.getReturnType().toString())
+      .replace("{receiver}", receiver)
+      .replace("{paramDecl}", paramDecl)
+      .replace("{paramNames}", paramNames);
+
+    return methodTemplate;
+  }
+
+  private String getFields(TypeElement element) {
+    String result = "final {loggerType} $testLogger$;".replace("{loggerType}", TestLogger.class.getSimpleName());
+
+    if (proxiesOnlyStaticMethods(element)) {
+      return result;
+    }
+
+    String innerField = "final {innerType} $inner$;".replace("{innerType}", element.getSimpleName());
+    return result + "\n" + innerField;
+  }
+
+  private boolean proxiesOnlyStaticMethods(TypeElement element) {
+    return getProxiedMethods(element).stream().allMatch(it -> it.getModifiers().contains(Modifier.STATIC));
+  }
+
+  private String getConstructor(TypeElement element) {
+    String template = proxiesOnlyStaticMethods(element)
+      ? CONSTRUCTOR_TEMPLATE_WITHOUT_INNER
+      : CONSTRUCTOR_TEMPLATE_WITH_INNER;
+    return template
+      .replace("{innerName}", element.getSimpleName())
+      .replace("{loggerType}", TestLogger.class.getSimpleName());
+  }
+
+}
